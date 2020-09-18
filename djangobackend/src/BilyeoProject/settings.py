8ebceaa9c6709226410a453459c03b0caabdd268
b00217 <b00217@naver.com>
2020-09-18 18:49:39 +0900

    test

--- djangobackend/src/BilyeoProject/settings.py
+++ djangobackend/src/BilyeoProject/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'BilyeoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'billyeodb',
+        'USER': 'admin',
+        'PASSWORD' : '123456789',
+        'HOST' : 'database-1.cx13lmsqnav9.us-east-2.rds.amazonaws.com',
+        'PORT' : '3306',
     }
 }
 
